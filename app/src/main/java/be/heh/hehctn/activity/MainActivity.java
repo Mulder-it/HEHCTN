ceaa0e03f4ef18e49e526852e1055765c383fe0a
Mulder <demulderjerome@gmail.com>
2022-08-08 15:14:10 +0200

    Initial commit

---
+++ app/src/main/java/be/heh/hehctn/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package be.heh.hehctn.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import be.heh.hehctn.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
